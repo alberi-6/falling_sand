f326f2db5db6de036f381b5fb139233f8943f2b4
alberi-6 <alle.96@live.se>
2024-07-13 16:29:39 +0200

    Add more events. Add board initializer

--- main.cpp
+++ main.cpp
@@ -2,10 +2,23 @@
 #include <SDL_image.h>
 
 #include <iostream>
+#include <vector>
 
 #include "RenderWindow.hpp"
 
+std::vector<int> initBoard(int width, int height) {
+    std::vector<int> board(width * height, 0);
+    return board;
+}
+
 int main(int argc, char* args[]) {
+    const int screenResolutionX = 1280;
+    const int screenResolutionY = 720;
+    const int scalingFactor = 10;
+
+    const int scaledResolutionX = screenResolutionX / scalingFactor;
+    const int scaledResolutionY = screenResolutionY / scalingFactor;
+
     const char* pGameTitle = "Falling Sand";
 
     // Initialize SDL
@@ -17,18 +30,30 @@ int main(int argc, char* args[]) {
         printf("IMG could not initialize! SDL_Error: %s\n", SDL_GetError());
     }
 
-    RenderWindow window(pGameTitle, 1280, 720);
-    window.setRenderScale(10, 10);
+    RenderWindow window(pGameTitle, screenResolutionX, screenResolutionY);
+    window.setRenderScale(scalingFactor, scalingFactor);
     window.clearWindow();
+
+    std::vector<int> gameBoard = initBoard(scaledResolutionX, scaledResolutionY);
+
+    std::cout << gameBoard.size() << std::endl;
 
     bool gameRunning = true;
 
     SDL_Event event;
-
     while (gameRunning) {
         while (SDL_PollEvent(&event)) {
-            if (event.type == SDL_QUIT) {
-                gameRunning = false;
+            switch (event.type) {
+                case SDL_QUIT:
+                    gameRunning = false;
+                    break;
+
+                case SDL_MOUSEMOTION:
+                    std::cout << "mouse moving" << std::endl;
+                case SDL_MOUSEBUTTONDOWN:
+                    std::cout << "mouse pressed" << std::endl;
+                default:
+                    break;
             }
         }
     }
