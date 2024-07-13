09f2bd8af07f83996858a2030645dfea51fdfb51
alberi-6 <alle.96@live.se>
2024-07-13 13:10:05 +0200

    Fix cmake and imports.

--- src/RenderWindow.cpp
+++ src/RenderWindow.cpp
@@ -1,7 +1,7 @@
 #include "RenderWindow.hpp"
 
-#include <SDL2/SDL.h>
-#include <SDL2/SDL_image.h>
+#include <SDL.h>
+#include <SDL_image.h>
 
 RenderWindow::RenderWindow(const char *p_Title, int p_Width, int p_Height)
     : window(NULL), renderer(NULL) {
